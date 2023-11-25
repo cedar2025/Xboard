25317d5f64ee14f534cdbde330c71f6aa717d121
xboard <xboard@xboard.one>
2023-11-25 10:24:14 +0800

    to: 添加前端静态资源请求版本号，防止更新导致的缓存

--- public/theme/Xboard/dashboard.blade.php
+++ public/theme/Xboard/dashboard.blade.php
@@ -1,34 +1,33 @@
 <!doctype html>
 <html lang="zh-CN">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"/>
-    <title>{{$title}}</title>
-    <script type="module" crossorigin src="/theme/{{$theme}}/assets/umi.js"></script>
-  </head>
-  <body>
-    
-    <script>
-        window.routerBase = "/";
-        window.settings = {
-            title: '{{$title}}',
-            assets_path: '/theme/{{$theme}}/assets',
-            version: '{{$version}}',
-            background_url: '{{$theme_config['background_url']}}',
-            description: '{{$description}}',
-            i18n: [
-                'zh-CN',
-                'en-US',
-                'ja-JP',
-                'vi-VN',
-                'ko-KR',
-                'zh-TW',
-                'fa-IR'
-            ],
-            logo: '{{$logo}}'
-        }
-    </script>
-    <div id="app"></div>
-    {!! $theme_config['custom_html'] !!}
-  </body>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
+  <title>{{$title}}</title>
+  <script type="module" crossorigin src="/theme/{{$theme}}/assets/umi.js?v={{$version}}"></script>
+</head>
+<body>
+  <script>
+    window.routerBase = "/";
+    window.settings = {
+      title: '{{$title}}',
+      assets_path: '/theme/{{$theme}}/assets',
+      version: '{{$version}}',
+      background_url: '{{$theme_config['background_url']}}',
+      description: '{{$description}}',
+      i18n: [
+        'zh-CN',
+        'en-US',
+        'ja-JP',
+        'vi-VN',
+        'ko-KR',
+        'zh-TW',
+        'fa-IR'
+      ],
+      logo: '{{$logo}}'
+    }
+  </script>
+  <div id="app"></div>
+  {!! $theme_config['custom_html'] !!}
+</body>
 </html>
