95675de30b5b194aee135aafe00732b75e129f27
xboard <xboard@xboard.one>
2024-01-10 18:59:59 +0800

    feat: [用户前端] 修复部分BUG、增加主题色、增加注册用户协议显示等

--- public/theme/Xboard/dashboard.blade.php
+++ public/theme/Xboard/dashboard.blade.php
@@ -1,17 +1,23 @@
 <!doctype html>
 <html lang="zh-CN">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
   <title>{{$title}}</title>
-  <script type="module" crossorigin src="/theme/{{$theme}}/assets/umi.js?v={{$version}}"></script>
+  <script type="module" crossorigin src="/theme/{{$theme}}/assets/umi.js"></script>
 </head>
+
 <body>
+
   <script>
     window.routerBase = "/";
     window.settings = {
       title: '{{$title}}',
       assets_path: '/theme/{{$theme}}/assets',
+      theme: {
+          color: '{{$theme_config['theme_color']}}',
+      },
       version: '{{$version}}',
       background_url: '{{$theme_config['background_url']}}',
       description: '{{$description}}',
@@ -30,4 +36,5 @@
   <div id="app"></div>
   {!! $theme_config['custom_html'] !!}
 </body>
+
 </html>
