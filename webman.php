bb708d608434421e2e16691a240e7bbdc983c2d4
xboard <xboard@xboard.one>
2024-04-07 22:23:34 +0800

    chore: 增加通过环境变量WEBMAN_WORKERS修改Webman的worker数的功能

--- webman.php
+++ webman.php
@@ -7,12 +7,12 @@ use Workerman\Worker;
 Adapterman::init();
 
 $http_worker                = new Worker('http://127.0.0.1:7010');
-$http_worker->count         = swoole_cpu_num() ?? 2;
+$http_worker->count         = getenv('WEBMAN_WORKERS') ?? (swoole_cpu_num() ?? 1);
 $http_worker->name          = 'AdapterMan';
 
 $http_worker->onWorkerStart = static function () {
     //init();
-    require __DIR__.'/start.php';
+    require __DIR__ . '/start.php';
 };
 
 $http_worker->onMessage = static function ($connection, $request) {
