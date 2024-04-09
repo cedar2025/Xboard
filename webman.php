880e5eb29d1ab72a2448f421f4d508d81d22d399
Xboard <147830212+cedar2025@users.noreply.github.com>
2024-04-09 22:52:10 +0800

    Update webman.php

--- webman.php
+++ webman.php
@@ -7,7 +7,7 @@ use Workerman\Worker;
 Adapterman::init();
 
 $http_worker                = new Worker('http://127.0.0.1:7010');
-$http_worker->count         = getenv('WEBMAN_WORKERS') ?? (swoole_cpu_num() ?? 1);
+$http_worker->count         = getenv('WEBMAN_WORKERS') ?: (swoole_cpu_num() ?: 1);
 $http_worker->name          = 'AdapterMan';
 
 $http_worker->onWorkerStart = static function () {
