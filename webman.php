a3abb62728c0fb250be73661a81da3d8a9a8ea3e
xboard <147830212+cedar2025@users.noreply.github.com>
2024-05-14 18:10:15 +0800

    chore: 优化docker环境下的启动速度，增加webman进程自动重启来释放内存

--- webman.php
+++ webman.php
@@ -4,11 +4,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Adapterman\Adapterman;
 use Workerman\Worker;
+
 Adapterman::init();
 
-$http_worker                = new Worker('http://127.0.0.1:7010');
-$http_worker->count         = getenv('WEBMAN_WORKERS') ?: (swoole_cpu_num() ?: 1);
-$http_worker->name          = 'AdapterMan';
+$http_worker = new Worker('http://127.0.0.1:7010');
+$http_worker->count = getenv('WEBMAN_WORKERS') ?: max(swoole_cpu_num(), 2);
+$http_worker->name = 'AdapterMan';
 
 $http_worker->onWorkerStart = static function () {
     //init();
@@ -17,7 +18,12 @@ $http_worker->onWorkerStart = static function () {
 
 $http_worker->onMessage = static function ($connection, $request) {
 
+    static $request_count;
+
     $connection->send(run());
+    if (++$request_count > 10000) {
+        Worker::stopAll();
+    }
 };
 
 Worker::runAll();
