e6e6b39747072515196b846e5a61bbde2d0cb20a
Elysia <118418714+elysias123@users.noreply.github.com>
2025-01-12 15:31:51 +0800

    feat: allow admin/staff unbind a user

--- app/Plugins/Telegram/Commands/UnBind.php
+++ app/Plugins/Telegram/Commands/UnBind.php
@@ -12,11 +12,17 @@ class UnBind extends Telegram {
 
     public function handle($message, $match = []) {
         if (!$message->is_private) return;
-        $user = User::where('telegram_id', $message->chat_id)->first();
-        $telegramService = $this->telegramService;
-        if (!$user) {
-            $telegramService->sendMessage($message->chat_id, '没有查询到您的用户信息，请先绑定账号', 'markdown');
-            return;
+        if (!isset($message->args[0])) {
+            $user = User::where('telegram_id', $message->chat_id)->first();
+        } else {
+            $chat = User::where('telegram_id', $message->chat_id)->first();
+            if (!$chat) return;
+            if (!($chat->is_admin || $chat->is_staff)) return;
+            if (strpos($message->args[0], '@') !== true) {
+                $user = User::where('email', $message->args[0])->first();
+            } else {
+                $user = User::where('telegram_id', $message->args[0])->first();
+            }
         }
         $user->telegram_id = NULL;
         if (!$user->save()) {
