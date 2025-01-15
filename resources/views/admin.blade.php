d57737662669a31668a4a9d9cca439bba3cd0454
xboard <xboard@xboard.one>
2025-01-15 18:00:32 +0800

    fix(admin): correct know file issues

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -17,6 +17,9 @@
   <script type="module" crossorigin src="/assets/admin/assets/index.js"></script>
   <link rel="stylesheet" crossorigin href="/assets/admin/assets/index.css" />
   <link rel="stylesheet" crossorigin href="/assets/admin/assets/vendor.css">
+  <script src="/assets/admin/locales/en-US.js"></script>
+  <script src="/assets/admin/locales/zh-CN.js"></script>
+  <script src="/assets/admin/locales/ko-KR.js"></script>
 </head>
 
 <body>
