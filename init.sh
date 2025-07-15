ac08199580c3bcf4bad3be66f2926619a699b25d
xboard <xboard@xboard.one>
2025-07-15 23:09:38 +0800

    fix: fix Redis RDB snapshot permission error

--- init.sh
+++ init.sh
@@ -8,3 +8,7 @@ php artisan xboard:install
 if [ -f "/etc/init.d/bt" ] || [ -f "/.dockerenv" ]; then
   chown -R www:www $(pwd);
 fi
+
+if [ -d ".docker/.data" ]; then
+  chmod -R 777 .docker/.data
+fi
