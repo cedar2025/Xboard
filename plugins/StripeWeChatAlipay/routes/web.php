0b3fb7b8f72eefde01a5d497887dd7482665326b
realzuanzuan <shaozuanzuan@gmail.com>
2026-01-09 23:29:38 +0800

    remove stripewechatalipay pay method

--- plugins/StripeWeChatAlipay/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Plugin\StripeWeChatAlipay\Controllers\PaymentController;
-
-Route::prefix('plugins/stripe-wechat-alipay')->group(function () {
-    Route::get('/payment', [PaymentController::class, 'show'])->name('stripe-wechat-alipay.payment');
-});
