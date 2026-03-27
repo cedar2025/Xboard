7af07640317b25e4aafe170e8e3dbe2561a7ade0
xboard <147830212+cedar2025@users.noreply.github.com>
2026-03-28 07:07:57 +0800

    feat: implement email case-insensitive queries (fix #318)

--- app/Services/Auth/LoginService.php
+++ app/Services/Auth/LoginService.php
@@ -36,7 +36,7 @@ class LoginService
         }
 
         // 查找用户
-        $user = User::where('email', $email)->first();
+        $user = User::byEmail($email)->first();
         if (!$user) {
             return [false, [400, __('Incorrect email or password')]];
         }
@@ -99,7 +99,7 @@ class LoginService
         }
 
         // 查找用户
-        $user = User::where('email', $email)->first();
+        $user = User::byEmail($email)->first();
         if (!$user) {
             return [false, [400, __('This email is not registered in the system')]];
         }
