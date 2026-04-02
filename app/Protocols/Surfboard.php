06ff341290b20bec94ae73bcec39bd3d9be845be
NFamou <nuwaago@gmail.com>
2026-04-02 15:46:55 +0800

    Merge pull request #856 from NFamou/master
    支持Surfboard下发SS2022

--- app/Protocols/Surfboard.php
+++ app/Protocols/Surfboard.php
@@ -36,7 +36,10 @@ class Surfboard extends AbstractProtocol
                     'aes-128-gcm',
                     'aes-192-gcm',
                     'aes-256-gcm',
-                    'chacha20-ietf-poly1305'
+                    'chacha20-ietf-poly1305',
+                    '2022-blake3-aes-128-gcm',
+                    '2022-blake3-aes-256-gcm',
+                    '2022-blake3-chacha20-poly1305'      
                 ])
             ) {
                 // [Proxy]
