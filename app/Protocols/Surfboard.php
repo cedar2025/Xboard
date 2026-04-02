c84d995c540c13b87c16129f742bd09f0acfbd88
liujang <41243924+liujang@users.noreply.github.com>
2026-04-02 23:40:07 +0800

    Update Surfboard.php

--- app/Protocols/Surfboard.php
+++ app/Protocols/Surfboard.php
@@ -69,7 +69,7 @@ class Surfboard extends AbstractProtocol
         $config = subscribe_template('surfboard');
         // Subscription link
         $subsURL = Helper::getSubscribeUrl($user['token']);
-        $subsDomain = request()->header('Host');
+        $subsDomain = request()->header('X-Subs-Domain');
 
         $config = str_replace('$subs_link', $subsURL, $config);
         $config = str_replace('$subs_domain', $subsDomain, $config);
