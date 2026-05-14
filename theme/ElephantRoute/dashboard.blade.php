cd8163bf3cd096bbe7befd89539c39f267603e69
joyefrck <changyongcai30@gmail.com>
2026-05-15 01:10:59 +0800

    重新修复高度

--- theme/ElephantRoute/dashboard.blade.php
+++ theme/ElephantRoute/dashboard.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
   <title>{{$title}}</title>
-  <link rel="stylesheet" href="/theme/{{$theme}}/assets/elephant-route-auth.css?v={{$version}}-er20260515c">
+  <link rel="stylesheet" href="/theme/{{$theme}}/assets/elephant-route-auth.css?v={{$version}}-er20260515d">
   <link rel="stylesheet" href="/theme/{{$theme}}/assets/elephant-route-dashboard.css?v={{$version}}-er20260515a">
   <script type="module" crossorigin src="/theme/{{$theme}}/assets/umi.js"></script>
   
