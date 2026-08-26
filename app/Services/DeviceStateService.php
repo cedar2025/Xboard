01fc7c513467513d8f72fbb283e4c80a63c618df
asukassa <117507979+asukassa@users.noreply.github.com>
2026-08-26 09:44:22 +0800

    fix: reindex device IP array before JSON encoding

--- app/Services/DeviceStateService.php
+++ app/Services/DeviceStateService.php
@@ -162,7 +162,7 @@ class DeviceStateService
                     }
                 }
                 if (!empty($ips)) {
-                    $result[$userId] = array_unique($ips);
+                    $result[$userId] = array_values(array_unique($ips));
                 }
             }
         }
